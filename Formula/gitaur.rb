c9870f71278cb0bd5642be62781c632a0d8a3955
Vishok M <vishokmanikantan@gmail.com>
2026-06-06 09:40:04 +0530

    Fix URL case sensitivity in gitaur.rb

--- Formula/gitaur.rb
+++ Formula/gitaur.rb
@@ -1,7 +1,7 @@
 class Gitaur < Formula
   desc "A simple resume utility CLI tool"
   homepage "https://github.com/Exceptionate/gitaur"
-  url "https://github.com/Exceptionate/gitaur/releases/download/v0.1.0/gitaur-macos-arm64.tar.gz"
+  url "https://github.com/exceptionate/gitaur/releases/download/v0.1.0/gitaur-macos-arm64.tar.gz"
   version "0.1.0"
   sha256 "c2ae3efb92cf1ba40f6f6943690abfd5fa4deac2f082c09b5698cd2dbcb50859"
 
